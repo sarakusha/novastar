0b139517155823afeace755b7ddb6315655240ff
Andrei Sarakeev <sarakusha@gmail.com>
2021-06-07 14:05:23 +0300

    chore: upgrade dependencies

--- packages/codec/src/lib/packet.spec.ts
+++ packages/codec/src/lib/packet.spec.ts
@@ -12,52 +12,13 @@ describe('packs', () => {
     req.address = 0x02000001;
     req.updateCrc();
     const check = [
-      0x55,
-      0xaa,
-      0,
-      0x15,
-      0xfe,
-      0,
-      1,
-      0,
-      0,
-      0,
-      1,
-      0,
-      1,
-      0,
-      0,
-      2,
-      1,
-      0,
-      0x80,
-      0xee,
-      0x56,
+      0x55, 0xaa, 0, 0x15, 0xfe, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 2, 1, 0, 0x80, 0xee, 0x56,
     ];
     expect(Struct.raw(req)).toEqual(Buffer.from(check));
   });
   test('AcknowledgePack', () => {
     const data = Buffer.from([
-      0xaa,
-      0x55,
-      0,
-      0x15,
-      0,
-      0xfe,
-      1,
-      0,
-      0,
-      0,
-      1,
-      0,
-      1,
-      0,
-      0,
-      2,
-      0,
-      0,
-      0x6d,
-      0x56,
+      0xaa, 0x55, 0, 0x15, 0, 0xfe, 1, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 0x6d, 0x56,
     ]);
     const pack = new Packet(data);
     expect(pack.ack === 0 && pack.serno === 0x15 && pack.address === 0x2000001);
