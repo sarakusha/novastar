8c5a1a92c3fa898c176b3a5ddf3e96271fbe3e06
sarakusha <sarakusha@gmail.com>
2022-02-08 10:16:05 +0300

    test: updated

--- packages/codec/src/lib/NovastarDecoder.spec.ts
+++ packages/codec/src/lib/NovastarDecoder.spec.ts
@@ -1,5 +1,5 @@
 import NovastarDecoder from './NovastarDecoder';
-import { COMPUTER, getCrc, Packet, RESPONSE } from './Packet';
+import { COMPUTER, Packet, RESPONSE } from './Packet';
 
 describe('NovastarDecoder', () => {
   it('decode', done => {
@@ -12,7 +12,7 @@ describe('NovastarDecoder', () => {
     res.length = data.length;
     res.data.write(data);
     const raw = Packet.raw(res);
-    if (raw) res.crc = getCrc(raw);
+    Packet.crc(res, true);
     const decoder = new NovastarDecoder();
     decoder.once('data', chunk => {
       expect(chunk).toEqual(res);
@@ -20,4 +20,14 @@ describe('NovastarDecoder', () => {
     });
     decoder.write(raw);
   });
+  it('decode hex', () => {
+    const raw = Buffer.from('55AA007AFEFF00000000010000000001010000CF57', 'hex');
+    const packet = new Packet(raw);
+    expect(packet.crc).toBe(Packet.crc(packet));
+    // console.log({ packet: packet.toJSON() });
+  });
+  // it('crypto', () => {
+  //   const key = [106, 51, 25, 141, 157, 142, 23, 111, 234, 159, 187, 154, 215, 34, 37, 205];
+  //   console.log(Buffer.from(key).toString());
+  // });
 });
--- packages/codec/src/lib/packet.spec.ts
+++ packages/codec/src/lib/packet.spec.ts
@@ -4,13 +4,16 @@ import { DeviceType, Packet } from './Packet';
 import Request from './Request';
 
 describe('packs', () => {
+  test('Packet', () => {
+    expect(Packet.baseSize).toBe(20);
+  });
   test('RequestPack', () => {
     const req = new Request([0x80]);
     req.serno = 0x15;
     req.destination = 0;
     req.deviceType = DeviceType.ReceivingCard;
     req.address = 0x02000001;
-    req.updateCrc();
+    Request.crc(req, true);
     const check = [
       0x55, 0xaa, 0, 0x15, 0xfe, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 2, 1, 0, 0x80, 0xee, 0x56,
     ];
