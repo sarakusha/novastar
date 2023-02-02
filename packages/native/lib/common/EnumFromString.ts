c67851f0ba3264396c88039811c97ed8c5d12b36
sarakusha <sarakusha@gmail.com>
2023-02-02 09:50:57 +0300

    refactor: parameter type refinement

--- packages/native/lib/common/EnumFromString.ts
+++ packages/native/lib/common/EnumFromString.ts
@@ -2,7 +2,7 @@ import { chain } from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 
-const EnumFromString = <T>(enumObj: T, enumName = 'enum') =>
+const EnumFromString = <T extends Record<string, unknown>>(enumObj: T, enumName = 'enum') =>
   new t.Type<T[keyof T], keyof T>(
     enumName,
     (u): u is T[keyof T] => typeof u === 'number' && Object.values(enumObj).includes(u),
@@ -10,7 +10,7 @@ const EnumFromString = <T>(enumObj: T, enumName = 'enum') =>
       typeof u === 'number' && Object.values(enumObj).includes(u)
         ? t.success(u as unknown as T[keyof T])
         : pipe(
-          t.keyof(enumObj as Record<string, unknown>).validate(u, c),
+          t.keyof(enumObj).validate(u, c),
           chain(s => t.success(enumObj[s as keyof T])),
         ),
     a => enumObj[a as unknown as keyof T] as unknown as keyof T,
