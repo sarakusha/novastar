2a2d04ca501f583cd63c606b4452911a55c5ceb0
Andrei Sarakeev <sarakusha@gmail.com>
2025-04-28 08:47:18 +0300

    test:

--- packages/codec/src/packet.spec.ts
+++ packages/codec/src/packet.spec.ts
@@ -13,9 +13,11 @@ describe('packs', () => {
     req.destination = 0;
     req.deviceType = DeviceType.ReceivingCard;
     req.address = 0x02000001;
+    req.port = 0x11;
+    req.rcvIndex = 0x234;
     Request.crc(req, true);
     const check = [
-      0x55, 0xaa, 0, 0x15, 0xfe, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 2, 1, 0, 0x80, 0xee, 0x56,
+      0x55, 0xaa, 0, 0x15, 0xfe, 0, 1, 0x11, 0x34, 0x2, 1, 0, 1, 0, 0, 2, 1, 0, 0x80, 0x35, 0x57,
     ];
     expect(Struct.raw(req)).toEqual(Buffer.from(check));
   });
